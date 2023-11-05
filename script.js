c5f27227928d17bb4447fb0e695f45ae025fd2f3
Piotr Poterek <peterpoterek0@gmail.com>
2023-11-05 17:13:56 +0100

    mobile menu toggle

--- script.js
+++ script.js
@@ -21,3 +21,27 @@ const swiper = new Swiper(".swiper", {
     el: ".swiper-scrollbar",
   },
 });
+
+(() => {
+  const mobileMenu = document.querySelector("#mobile-menu");
+  const openMenuBtn = document.querySelector("#mobile-open");
+  const closeMenuBtn = document.querySelector("#mobile-close");
+
+  console.log(mobileMenu, openMenuBtn, closeMenuBtn);
+
+  const toggleMenu = () => {
+    console.log("test");
+    const isMenuOpen = openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+    openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
+    mobileMenu.classList.toggle("is-open");
+  };
+
+  openMenuBtn.addEventListener("click", toggleMenu);
+  closeMenuBtn.addEventListener("click", toggleMenu);
+
+  window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove("is-open");
+    openMenuBtn.setAttribute("aria-expanded", false);
+  });
+})();
